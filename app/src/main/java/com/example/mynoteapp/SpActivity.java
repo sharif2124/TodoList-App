a5a1d262abad3dc0ee85bc4a39a008cb13fb5573
sharif24 <azizur15-2124@diu.edu.bd>
2022-07-15 22:17:27 -0700

    complete

---
+++ app/src/main/java/com/example/mynoteapp/SpActivity.java
@@ -0,0 +1,24 @@
+package com.example.mynoteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sp);
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            startActivity(new Intent(SpActivity.this,MainActivity.class));
+            }
+        },3000);
+    }
+}
